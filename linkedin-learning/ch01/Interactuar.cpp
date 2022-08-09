8dce0990c7b888086c736371fd6982ebe292a8db
gaspar <you@example.com>
2022-08-09 13:02:36 -0600

    interactua

---
+++ linkedin-learning/ch01/Interactuar.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main(){
+	std::string str;
+	std::cout<<"Nombre: ";
+	std::cin>>str;
+	std::cout<<"el nombre es: "<<str;
+}
--- linkedin-learning/ch01/Primero.cpp
+++ linkedin-learning/ch01/Primero.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
 int main(){
 	std::cout<<"hola patito";
+	return (0);
 }
